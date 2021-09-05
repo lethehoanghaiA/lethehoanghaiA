56cbac9a8c95ff04795486a9efdf918e3c9e2a1d
hoanghailethe <hoanghailethe@users.noreply.github.com>
2021-09-05 16:52:31 +0700

    rename folder

--- BMI/js/bmi.js
+++ BMI/js/bmi.js
@@ -10,30 +10,29 @@ let bmi = null;
 buttonSubmit.addEventListener('click', function(){
     console.log('hello')
     let weight = inputWeight.value;
-    let height = inputHeight.value;
+    let height = inputHeight.value/100;
     bmi = (weight/(height*height));
     console.log(bmi);
 
     if(bmi<18.5) {
         leftPart.style.backgroundImage = "url('thin.jpg')";
         leftPart.style.backgroundSize = "contain";
-        leftPart.style.backgroundRepeat ="repeat";
-        leftPart.style.backgroundPosition = "25% 25%";
+        
         message.innerHTML = "";
         message.innerHTML += "<h2>Gravity can't hold you! Please eat more!</h2>"
     } else if (bmi>25) {
         leftPart.style.backgroundImage = "url('fat.jpg')";
         leftPart.style.backgroundSize = "contain";
-        leftPart.style.backgroundRepeat ="repeat";
-        leftPart.style.backgroundPosition = "25% 25%";
+       
         message.innerHTML = "";
         message.innerHTML += "<h2>No burger for you tonight!</h2>"
     } else {
         leftPart.style.backgroundImage = "url('fit.jpg')";
         leftPart.style.backgroundSize = "contain";
-        leftPart.style.backgroundRepeat ="repeat";
-        leftPart.style.backgroundPosition = "25% 25%";
+        
         message.innerHTML = "";
         message.innerHTML += "<h2>Congrat! You fit your pants!</h2>"
-        }
+    }
+    // leftPart.style.backgroundRepeat ="repeat";
+    // leftPart.style.backgroundPosition = "25% 25%";
 })
